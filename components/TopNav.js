d66f5a88ef74a3bdd8d492e896ec1fedbd8340b6
arfat-beep <arfatrahman08@gmail.com>
2022-09-16 00:27:38 +0600

    logout added

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,18 +1,37 @@
 import { Menu } from "antd";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   LoginOutlined,
   UserAddOutlined,
   AppstoreAddOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
+import { Context } from "../context";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+
 const { Item } = Menu;
 
 const TopNav = () => {
   const [current, setCurrent] = useState("");
+  const { state, dispatch } = useContext(Context);
+
+  const router = useRouter();
   useEffect(() => {
     process.browser && setCurrent(window.location.pathname);
   }, [process.browser && window.location.pathname]);
+
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    setCurrent("/login");
+    router.push("/login");
+  };
+
   return (
     <Menu mode="horizontal" selectedKeys={[current]} className="mb-2">
       <Item
@@ -33,17 +52,13 @@ const TopNav = () => {
           <a>Login</a>
         </Link>
       </Item>
-      <Item
-        icon={
-          <UserAddOutlined
-            key={"/register"}
-            onClick={(e) => setCurrent(e.key)}
-          />
-        }
-      >
+      <Item icon={<UserAddOutlined />} key={"/register"}>
         <Link href={"/register"}>
           <a>Register</a>
         </Link>
+      </Item>
+      <Item onClick={logout} className="float-end" icon={<LogoutOutlined />}>
+        Logout
       </Item>
     </Menu>
   );
