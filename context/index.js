21707e90154d6740e0772e9fc20b623311ec4b36
arfat-beep <arfatrahman08@gmail.com>
2022-09-16 13:06:40 +0600

    handle expire token using axios interceptors and added CSURF token security

--- context/index.js
+++ context/index.js
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
-
+import { useRouter } from "next/router";
+import axios from "axios";
 // initial state
 const initialState = {
   user: null,
@@ -24,11 +25,50 @@ const rootReducer = (state, action) => {
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, initialState);
 
+  const router = useRouter();
+
   useEffect(() => {
     dispatch({
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
+  }, []);
+
+  axios.interceptors.response.use(
+    function (response) {
+      // any status code that lie within the range of 2XX cause this function to trigger
+      return response;
+    },
+    function (error) {
+      // any status codes that falls outside the range of 2XX cause this function to trigger
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              console.log("/402 error > logout");
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log("AXIOS INTERCEPTORS ERR", err);
+              reject(err);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      // console.log("CSRF", data);
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
   }, []);
 
   return (
