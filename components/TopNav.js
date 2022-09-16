5e34163ef8770000a8bc1863813357a52879e974
arfat-beep <arfatrahman08@gmail.com>
2022-09-16 10:28:57 +0600

    conditional nav links added

--- components/TopNav.js
+++ components/TopNav.js
@@ -6,17 +6,21 @@ import {
   UserAddOutlined,
   AppstoreAddOutlined,
   LogoutOutlined,
+  CoffeeOutlined,
 } from "@ant-design/icons";
 import { Context } from "../context";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 
-const { Item } = Menu;
+const { Item, SubMenu } = Menu;
 
 const TopNav = () => {
   const [current, setCurrent] = useState("");
+
+  // context state
   const { state, dispatch } = useContext(Context);
+  const { user } = state;
 
   const router = useRouter();
   useEffect(() => {
@@ -33,7 +37,7 @@ const TopNav = () => {
   };
 
   return (
-    <Menu mode="horizontal" selectedKeys={[current]} className="mb-2">
+    <Menu mode="horizontal" selectedKeys={[current]} className="mb-2 mx-md-5">
       <Item
         key={"/"}
         onClick={(e) => setCurrent(e.key)}
@@ -43,23 +47,39 @@ const TopNav = () => {
           <a>app</a>
         </Link>
       </Item>
-      <Item
-        key={"/login"}
-        onClick={(e) => setCurrent(e.key)}
-        icon={<LoginOutlined />}
-      >
-        <Link href={"/login"}>
-          <a>Login</a>
-        </Link>
-      </Item>
-      <Item icon={<UserAddOutlined />} key={"/register"}>
-        <Link href={"/register"}>
-          <a>Register</a>
-        </Link>
-      </Item>
-      <Item onClick={logout} className="float-end" icon={<LogoutOutlined />}>
-        Logout
-      </Item>
+      {user === null && (
+        <>
+          <Item
+            key={"/login"}
+            onClick={(e) => setCurrent(e.key)}
+            icon={<LoginOutlined />}
+          >
+            <Link href={"/login"}>
+              <a>Login</a>
+            </Link>
+          </Item>
+          <Item icon={<UserAddOutlined />} key={"/register"}>
+            <Link href={"/register"}>
+              <a>Register</a>
+            </Link>
+          </Item>
+        </>
+      )}
+      {user !== null && (
+        <SubMenu
+          style={{ marginLeft: "auto" }}
+          icon={<CoffeeOutlined />}
+          title={user && user.name}
+        >
+          <Item
+            onClick={logout}
+            className="text-center"
+            icon={<LogoutOutlined />}
+          >
+            Logout
+          </Item>
+        </SubMenu>
+      )}
     </Menu>
   );
 };
