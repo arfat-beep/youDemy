436a77de01f26f827565b28dddb68a5642e47ece
arfat-beep <arfatrahman08@gmail.com>
2022-09-18 22:01:07 +0600

    forgot password front end complete

---
+++ pages/forgot-password.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import Router, { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../context";
+import { SyncOutlined } from "@ant-design/icons";
+
+const ForgotPassword = () => {
+  // state
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState("");
+  const [code, setCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  //   router
+  const router = useRouter();
+
+  //   context
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  //   redirect if user is logged in
+  useEffect(() => {
+    if (user !== null) router.push("/");
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/forgot-password", { email });
+    } catch (e) {
+      console.log("Error from forget password handleSubmit", e);
+      setLoading(false);
+      toast(e.response.data);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary">Forgot password</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control mb-4 p-4"
+            placeholder="Enter email"
+            required
+          />
+          <br />
+          <button
+            className="btn btn-primary btn-block p-2"
+            disabled={loading || !email}
+          >
+            {loading ? <SyncOutlined spin /> : "Submit"}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
--- pages/login.js
+++ pages/login.js
@@ -98,6 +98,12 @@ const login = () => {
             <a>Register</a>
           </Link>
         </p>
+        <p className="text-center ">
+          {" "}
+          <Link href={"/forgot-password"}>
+            <a className="text-danger">Forget password</a>
+          </Link>
+        </p>
       </div>
     </>
   );
