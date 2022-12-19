d0806e063fb78f40061c28c5438cb58d1fd8b27a
arfat-xyz <arfatrahman08@gmail.com>
2022-12-19 23:54:36 +0600

    modal for question generation added2

--- components/extraComponent/QuestGenInfoModal.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import { Button, Modal } from "antd";
-const App = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  return <></>;
-};
-export default App;
